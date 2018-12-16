2dbb849e7e5414962e272ccacb7916b989c6c3ae
Rafael Torres - MacTouch <rafaelfeitozatorres@gmail.com>
2018-12-16 00:59:53 -0200

    Estrutura do Sass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./scss/app.component.scss']
 })
 export class AppComponent {
   title = 'My First Angular App';
