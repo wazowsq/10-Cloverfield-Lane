bf52232cd495023de15f76fcddbb1160ee4a672a
Rafael Torres - MacTouch <rafaelfeitozatorres@gmail.com>
2018-12-16 01:16:49 -0200

    Ajuste na organização do sass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./scss/app.component.scss']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'My First Angular App';
